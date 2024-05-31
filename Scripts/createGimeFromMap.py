ac451268ff853ec124fe9b2289937f4c289a2996
ichir0roie <ichir0roie@gmail.com>
2024-05-31 22:27:00 +0900

    update additional space

--- Scripts/createGimeFromMap.py
+++ Scripts/createGimeFromMap.py
@@ -1,42 +1,37 @@
 
 
-
-
-
 class createGimeFormMap:
     def __init__(self):
-        self.gimes=[]
-        self.gimeCombined=[]
+        self.gimes = []
+        self.gimeCombined = []
         return
-    
-    def loadGime(self,fileName:str):
-        path="Maps/"+fileName+".tsv"
-        with open(path,mode="r",encoding="utf-8")as f:
-            data=f.read()
-            pairs=data.split("\n")
-        gime={}
+
+    def loadGime(self, fileName: str):
+        path = "Maps/"+fileName+".tsv"
+        with open(path, mode="r", encoding="utf-8")as f:
+            data = f.read()
+            pairs = data.split("\n")
+        gime = {}
         for pair in pairs:
             if not "\t" in pair:
                 continue
-            key,value=pair.split("\t")
-            gime[key]=value
-        
-        return gime
-    
+            key, value = pair.split("\t")
+            gime[key] = value
 
-    def loadGimes(self,fileNames:list):
-        self.gimes=[]
+        return gime
+
+    def loadGimes(self, fileNames: list):
+        self.gimes = []
         for fileName in fileNames:
-            gime=self.loadGime(fileName)
+            gime = self.loadGime(fileName)
             self.gimes.append(gime)
 
-    
     def combineGimes(self):
-        self.gimeCombined=[]
+        self.gimeCombined = []
 
         for gime in self.gimes:
             for key in gime.keys():
-                self.gimeCombined.append([key,gime[key]])
+                self.gimeCombined.append([key, gime[key]])
             self.gimeCombined.append(None)
 
     def print(self):
@@ -44,35 +39,36 @@ class createGimeFormMap:
             if item is None:
                 print("\n")
                 continue
-            key=item[0]
-            value=item[1]
-            print("{}\t:\t{}".format(key,value))
-    
-    def save(self,mapName:str):
-        text=""
+            key = item[0]
+            value = item[1]
+            print("{}\t:\t{}".format(key, value))
+
+    def save(self, mapName: str):
+        text = ""
         for item in self.gimeCombined:
-                if item is None:
-                    text+="\n"
-                    continue
-                text+="{}\t{}".format(item[0],item[1])+"\n"
+            if item is None:
+                text += "\n"
+                continue
+            text += "{}\t{}".format(item[0], item[1])+"\n"
 
-        with open("../google ime.tsv",mode="w",encoding="utf-8")as f:
+        with open("../google ime.tsv", mode="w", encoding="utf-8")as f:
             f.write(text)
 
-        with open("Output/{}.tsv".format(mapName),mode="w",encoding="utf-8")as f:
+        with open("Output/{}.tsv".format(mapName), mode="w", encoding="utf-8")as f:
             f.write(text)
 
-    
-if __name__=="__main__":
-    cgf=createGimeFormMap()
-    gimeList=[
+
+if __name__ == "__main__":
+    cgf = createGimeFormMap()
+    gimeList = [
         # "getaroBase",
         "getaroBaseDeleteAt",
         # "getaroShift",
         "getaroShiftEcoBase",
         # "getaroShiftEcoEasy",
         # "getaroSpecial",
-        "getaroSpecialShiftSL",
+        # "getaroSpecialShiftSL",
+        "getaroSpecialShiftYou",
     ]
     cgf.loadGimes(gimeList)
     cgf.combineGimes()
